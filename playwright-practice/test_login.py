7431ee835da7610890273eac1cae9bcc57bda310
Ioan Manolache <mioan22072003@gmail.com>
2026-09-18 22:34:42 +0200

    Add first Playwright script: login test with assertions

---
+++ playwright-practice/test_login.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+
+    page.goto("https://the-internet.herokuapp.com/login")
+    page.fill("#username", "tomsmith")
+    page.fill("#password", "SuperSecretPassword!")
+    page.click("button[type='submit']")
+
+    message = page.inner_text(".flash")
+    assert "secure" in message
+    assert "invalid" not in message
+
+    browser.close()
